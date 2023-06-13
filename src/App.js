0c290d02e5a375efdc13aa897a93e2051f9290a8
Will Brodhead <brodheadw@gmail.com>
2023-06-13 15:28:37 -0400

    newpage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>William Brodhead</h1>
-        <nav>
-          <button onClick={() => { console.log('You clicked Home!') }}>Home</button>
-          <button onClick={() => { console.log('You clicked About!') }}>About</button>
-        </nav>
-      </header>
+        <header className="App-header">
+          <div className="top-bar">
+            <a id="site-title" href="/" data-animation-role="header-element">william brodhead</a>
+            <nav>
+              <button onClick={() => { window.location.href="/"; }}>home</button>
+              <button onClick={() => { console.log('You clicked About!') }}>about</button>
+            </nav>
+          </div>
+          <div className="centered-content">
+            <div className="hero-section">
+              {/* other content */}
+            </div>
+          </div>
+        </header>
     </div>
   );
-}
-
-export default App;
+} export default App;
