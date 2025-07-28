d5e0c011528045362a47107f3ca5c3229e8731e3
Will Brodhead <brodheadw@gmail.com>
2025-07-28 10:29:19 -0400

    Updated basePath, verified static export works

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/brodheadw.github.io",
-  assetPrefix: "/brodheadw.github.io/",
   images: {
     unoptimized: true,
   },
